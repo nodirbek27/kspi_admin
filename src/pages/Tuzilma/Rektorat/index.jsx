050bd80d91292f01e6811a11bf48b3d3801ed8e9
Azimjon <azimeliboev2000@gmail.com>
2024-06-04 14:19:38 +0500

    Uploaded Styled-component to project, add retorat candidate ++

--- src/pages/Tuzilma/Rektorat/index.jsx
+++ src/pages/Tuzilma/Rektorat/index.jsx
@@ -4,6 +4,29 @@ import { RxArrowTopRight } from "react-icons/rx";
 import { MdEdit, MdDelete } from "react-icons/md";
 
 const Rektorat = () => {
+    const faceData = [
+        {
+            id: 1,
+            name_uz:
+                "Rector asdczxczcxzcx bdfgbdgbfgbnfgb fd sdfsf fasdfaSD ASDASD S",
+            name_ru: "вдлы овадлыв фывдлжфлыдлвфж лывды",
+            name_en: "kds laksvb lda xcxc en",
+        },
+        {
+            id: 2,
+            name_uz:
+                "Yoshlar masalalari va ma'naviy-ma'rifiy ishlar bo'yicha prorektor",
+            name_ru: "вдлыовадлыв фывдлжфлы длвфж лывды",
+            name_en: "kdslaa vksl da xcxc",
+        },
+        {
+            id: 3,
+            name_uz: "Ilmiy ishlar",
+            name_ru: "вдлыовад лыв фывд лжфлыдлвфж лы вды",
+            name_en: "kdslaksldxczxcza xcxc",
+        },
+    ];
+
     const formik = useFormik({
         initialValues: {
             name_uz: "",
@@ -15,26 +38,29 @@ const Rektorat = () => {
         },
     });
 
-    const faceData = [
-        {
-            id: 1,
-            name_uz: "Rector asdczxczcxzcx bdfgbdgbfgbnfgb fd sdfsf fasdfaSD ASDASD S",
-            name_ru: "вдлы овадлыв фывдлжфлыдлвфж лывды",
-            name_en: "kds laksvb lda xcxc en",
+    const formik_2 = useFormik({
+        initialValues: {
+            markaz_id: "",
+            lavozim_uz: "",
+            lavozim_ru: "",
+            lavozim_en: "",
+            fish_uz: "",
+            fish_ru: "",
+            fish_en: "",
+            unvon_uz: "",
+            unvon_ru: "",
+            unvon_en: "",
+            qabul_soati_uz: "",
+            qabul_soati_ru: "",
+            qabul_soati_en: "",
+            telefon_nomer: "",
+            tg_username: "",
+            biografiya_uz: "",
         },
-        {
-            id: 2,
-            name_uz: "Yoshlar masalalari va ma'naviy-ma'rifiy ishlar bo'yicha prorektor",
-            name_ru: "вдлыовадлыв фывдлжфлы длвфж лывды",
-            name_en: "kdslaa vksl da xcxc",
+        onSubmit: (values) => {
+            console.log(values);
         },
-        {
-            id: 3,
-            name_uz: "Ilmiy ishlar",
-            name_ru: "вдлыовад лыв фывд лжфлыдлвфж лы вды",
-            name_en: "kdslaksldxczxcza xcxc",
-        },
-    ];
+    });
 
     return (
         <div className="flex justify-center">
@@ -93,43 +119,61 @@ const Rektorat = () => {
                     <h1 className="text-[1.2rem] font-medium">
                         Rektarat lavozimlari:
                     </h1>
-                    <ol className="list-decimal flex flex-col gap-2">
-                        {faceData.map(({ id, name_uz, name_ru, name_en }) => (
-                            <li className="border bg-gray-50 p-2" key={id}>
-                                <div className="flex justify-between items-center">
-                                    <p className=" inline-block">{name_uz}</p>
-                                    <div className="flex gap-2">
-                                        <button className="flex items-center gap-2 bg-blue-400 rounded-md py-1 px-4 text-white font-medium hover:bg-blue-600 active:scale-95">
-                                            <span>Tahrirlash</span>
-                                            <MdEdit />
-                                        </button>
-                                        <button className="flex items-center gap-2 bg-red-500 rounded-md py-1 px-4 text-white font-medium hover:bg-red-600 active:scale-95">
-                                            <span>Delete</span>
-                                            <MdDelete />
-                                        </button>
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
+                    <ol className="list-decimal flex flex-col gap-2 ps-4">
+                        {faceData?.length !== 0 && faceData ? (
+                            faceData?.map(
+                                ({ id, name_uz, name_ru, name_en }) => (
+                                    <li
+                                        className="border bg-gray-50 p-2"
+                                        key={id}
+                                    >
+                                        <div className="flex justify-between items-center">
+                                            <p className=" inline-block">
+                                                {name_uz}
+                                            </p>
+                                            <div className="flex gap-2">
+                                                <button className="flex items-center gap-2 bg-blue-400 rounded-md py-1 px-4 text-white font-medium hover:bg-blue-600 active:scale-95">
+                                                    <span>Tahrirlash</span>
+                                                    <MdEdit />
+                                                </button>
+                                                <button className="flex items-center gap-2 bg-red-500 rounded-md py-1 px-4 text-white font-medium hover:bg-red-600 active:scale-95">
+                                                    <span>Delete</span>
+                                                    <MdDelete />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </li>
+                                )
+                            )
+                        ) : (
+                            <div>Ma'lumot mavjud emas!</div>
+                        )}
                     </ol>
                 </div>
 
-                {/* <ol className="list-decimal">
-                    {faceData.map(({id, name_uz}) => (
-                        <li key={id}>
-                            <p>{name_uz}</p>
-                            <div className="flex gap-4">
-                                <button className="flex items-center gap-2 bg-red-600 rounded-md py-1 px-4 text-white font-medium">
-                                    Tahrirlash <MdEdit />
-                                </button>
-                                <button className="flex items-center gap-2 bg-red-600 rounded-md py-1 px-4 text-white font-medium">
-                                    Delete <MdDelete />
-                                </button>
-                            </div>
-                        </li>
-
-                    ))}
-                </ol> */}
+                <div>
+                    <h1 className="text-[1.2rem] font-medium">
+                        Rektarat lavozim nomzodlarini yaratish
+                    </h1>
+                    <div className="flex items-center gap-4">
+                        <select
+                            name="RectaratLavozim"
+                            id="RectaratLavozim"
+                            className="select select-bordered w-full max-w-xs"
+                            value={formik_2.values.markaz_id}
+                            onChange={formik_2.handleChange}
+                        >
+                            {faceData?.map((item) => (
+                                <option key={item.id} value={item.id}>
+                                    {item.name_uz}
+                                </option>
+                            ))}
+                        </select>
+                        <h2 className="text-red-600">
+                            Avval qaysi lavozimgaligini kiriting!
+                        </h2>
+                    </div>
+                </div>
             </div>
         </div>
     );

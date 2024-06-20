5b117fab7fe7f53088b6c7c505486a85a5309418
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2024-06-20 10:36:00 +0500

    Magistr++

--- src/pages/Abiturient/Magistratura/index.jsx
+++ src/pages/Abiturient/Magistratura/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import AbiturientMagistr from "../../../components/AbiturientMagistr";
 
 const Magistratura = () => {
     return (
         <div>
-            Magistratura
+            <AbiturientMagistr />
         </div>
     );
 };

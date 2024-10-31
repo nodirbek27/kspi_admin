e791356b896e2e9c859fe5ffdbfa412515766798
Azimjon-M <azimeliboev2000@gmail.com>
2024-10-31 16:08:47 +0500

    Institut Hujjatlar+

---
+++ src/services/hujjatORVMq.js
@@ -0,0 +1,19 @@
+import axiosInstance from "./index";
+
+const ep = "institut/vazirlar_mahkamasi_qarorlari/";
+
+const get = () => axiosInstance.get(ep);
+
+const post = (item) => {
+    return axiosInstance.post(`${ep}`, item);
+};
+const patch = (id, item) => {
+    return axiosInstance.patch(`${ep}${id}/`, item);
+};
+const del = (id) => {
+    return axiosInstance.delete(`${ep}${id}/`);
+};
+
+const APINormativHuj = { get, post, patch, del };
+
+export default APINormativHuj;
